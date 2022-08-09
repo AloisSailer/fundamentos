6f5cc6108587be425d6ac6b54c4d687d99d9f564
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-09 17:58:06 -0300

    sobre CSS modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './assets/components/Header'
 import { Post } from './Post'
+
+import './style.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Alois Sailer"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam amet vel totam voluptates debitis nesciunt quos corrupti! Accusantium laudantium ipsa vitae ratione, amet adipisci facilis nisi, corporis placeat, et quas?"
