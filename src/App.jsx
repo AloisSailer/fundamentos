a35473f52a0b108cedbf53703dd24d04cc8107a0
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-09 14:45:28 -0300

    aula sobre propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Alois Sailer"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam amet vel totam voluptates debitis nesciunt quos corrupti! Accusantium laudantium ipsa vitae ratione, amet adipisci facilis nisi, corporis placeat, et quas?"
+      />
+      <Post
+        author="Stephan Espíndola"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam amet vel totam voluptates debitis nesciunt quos corrupti!"
+      />
     </div>
   )
 }
