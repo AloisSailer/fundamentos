ca78afb72034683863a653703636af12897b4ffb
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-11 22:51:48 -0300

    criação sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from './assets/components/Header'
 import { Post } from './Post'
 
+import styles from './App.module.css'
+
 import './global.css'
+
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Alois Sailer"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam amet vel totam voluptates debitis nesciunt quos corrupti! Accusantium laudantium ipsa vitae ratione, amet adipisci facilis nisi, corporis placeat, et quas?"
-      />
-      <Post
-        author="Stephan Espíndola"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam amet vel totam voluptates debitis nesciunt quos corrupti!"
-      />
+
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+          <Post
+            author="Alois Sailer"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam amet vel totam voluptates debitis nesciunt quos corrupti! Accusantium laudantium ipsa vitae ratione, amet adipisci facilis nisi, corporis placeat, et quas?"
+          />
+          <Post
+            author="Stephan Espíndola"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam amet vel totam voluptates debitis nesciunt quos corrupti!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
