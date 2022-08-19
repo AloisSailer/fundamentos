76fa89a16d39af69f5b72d9d4ac196993f3fddbb
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-19 12:02:52 -0300

    criação do componente Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -8,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/AloisSailer.png" />
+        <Avatar src="https://github.com/AloisSailer.png" />
         <strong>Alois Sailer</strong>
         <span>Web Developer</span>
       </div>
